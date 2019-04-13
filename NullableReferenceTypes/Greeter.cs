320bead7015ced94466f4448752f1458fdcfcd43
Eric De Carufel <eric.decarufel@gmail.com>
2019-04-13 12:23:48 -0400

    Add project files.

---
+++ NullableReferenceTypes/Greeter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullableReferenceTypes
+{
+    class Greeter
+    {
+        public void SayHello(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello {firstName[0]}. {lastName}!");
+        }
+    }
+}
---
+++ NullableReferenceTypes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeter = new Greeter();
+            greeter.SayHello(null, "De Carufel");
+        }
+    }
+
+}
